ddd05dfb427814381de083a787a4ac53f34cacf2
jclundy <joseph.lundy456@gmail.com>
2020-12-05 17:56:51 -0500

    updates rosserial control to not have delay

--- rosserial_control/rosserial_control.ino
+++ rosserial_control/rosserial_control.ino
@@ -1,18 +1,21 @@
 #include <AccelStepper.h>
 #include <ros.h>
-#include <std_msgs/UInt16.h>
+#include <std_msgs/Int16.h>
 
 AccelStepper stepper(AccelStepper::DRIVER, 9, 8);
 
-void servo_cb( const std_msgs::UInt16& cmd_msg){
+void servo_cb( const std_msgs::Int16& cmd_msg){
   stepper.setSpeed(cmd_msg.data);
 }
 
 ros::NodeHandle nh;
-ros::Subscriber<std_msgs::UInt16> sub("speed", servo_cb);
+ros::Subscriber<std_msgs::Int16> sub("speed", servo_cb);
 
-std_msgs::UInt16 stepsMsg;
+std_msgs::Int16 stepsMsg;
 ros::Publisher stepsPub("steps", &stepsMsg);
+
+std_msgs::Int16 speedMsg;
+ros::Publisher speedPub("speedAck", &speedMsg);
 
 void setup() {
   stepper.setMaxSpeed(10000);
@@ -20,15 +23,23 @@ void setup() {
 
   nh.initNode();
   nh.advertise(stepsPub);
+  nh.advertise(speedPub);
   nh.subscribe(sub);
 }
-
+int count = 0;
 void loop() {
   // put your main code here, to run repeatedly:
-  nh.spinOnce();
-  stepper.runSpeed();
 
-  stepsMsg.data = stepper.currentPosition();
-  stepsPub.publish(&stepsMsg);
-  delay(1);
+  count++;
+  if (count > 100) {
+    nh.spinOnce();  
+    stepsMsg.data = stepper.currentPosition();
+    stepsPub.publish(&stepsMsg);
+  
+    speedMsg.data = stepper.speed();
+    speedPub.publish(&speedMsg);
+
+    count = 0;
+  }
+  stepper.runSpeed();
 }
--- rosserial_control/script.sh
+++ rosserial_control/script.sh
@@ -1,11 +1,13 @@
+source ~/catkin_ws/source devel/setup.bash
+
 roscore
 
 #for subscribing to msgs from arduino
 rosrun rosserial_python serial_node.py /dev/ttyACM0
 
-rostopic echo chatter steps
+rostopic echo steps
 
 #for publishing speed command
-rostopic pub speed std_msgs/UInt16  <angle>
+rostopic pub speed std_msgs/Int16  -- <speed>
 
 
