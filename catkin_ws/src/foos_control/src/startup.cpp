e10f50f114f3f0700187317e9d51686b958430c5
jclundy <joseph.lundy456@gmail.com>
2021-12-29 18:33:41 -0500

    adds logging, fixes compile errors

--- catkin_ws/src/foos_control/src/startup.cpp
+++ catkin_ws/src/foos_control/src/startup.cpp
@@ -1,5 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Int16.h"
+#include "std_msgs/Int8.h"
 
 typedef struct {
   bool waitingForUserSwitchPress;
@@ -9,7 +11,7 @@ typedef struct {
 
 calibration_progress_t calibrationProgress;
 
-const int16_t DEFAULT_SPEED = 5000;
+const int16_t DEFAULT_SPEED = 200;
 int16_t speedCmd = 0;
 
 typedef enum {
@@ -18,19 +20,18 @@ typedef enum {
   FORWARD = 1,
 } direction_t;
 
-void limitReachedCallBack(const std_msgs::Int8::ConstPtr& msg)
+void limitReachedCallBack(const std_msgs::Int8& msg)
 {
-
-  ROS_INFO("limit hit %f", msg.data);
   if(calibrationProgress.waitingForUserSwitchPress) {
   	if(msg.data == FORWARD) {
   		// user pressed forward switch, drive forward
   		speedCmd = DEFAULT_SPEED;
-  	} else {
+  	} else if(msg.data == REVERSE) {
   		// user pressed rear switch, driver reverse
   		speedCmd = -1 * DEFAULT_SPEED;
   	}
   	calibrationProgress.waitingForUserSwitchPress = false;
+  	ROS_INFO("calibration started");
   } else if (msg.data == FORWARD && !calibrationProgress.forwardDepressed) {
   	// was driving forward and hit limit
   	calibrationProgress.forwardDepressed = true;
@@ -42,16 +43,25 @@ void limitReachedCallBack(const std_msgs::Int8::ConstPtr& msg)
 	// drive forward
 	speedCmd = DEFAULT_SPEED;
   }
-  
+    
   if(calibrationProgress.rearDepressed && calibrationProgress.forwardDepressed) {
   	speedCmd = 0;
-  }  
-
+  	ROS_INFO("done calibrating");
+  }
+  
+  ROS_INFO("limit hit %i", msg.data);    
+  ROS_INFO("speed cmd %i", speedCmd);  
+  ROS_INFO("forward pressed %i", calibrationProgress.forwardDepressed);
+  ROS_INFO("rear pressed %i", calibrationProgress.rearDepressed);
 }
 
 
 int main(int argc, char **argv)
 {
+
+  calibrationProgress.waitingForUserSwitchPress = true;
+  calibrationProgress.forwardDepressed = false;
+  calibrationProgress.rearDepressed = false;
 
   ros::init(argc, argv, "startup");
   ros::NodeHandle n;
@@ -59,10 +69,10 @@ int main(int argc, char **argv)
   ros::Subscriber sub = n.subscribe("limit_reached", 10, limitReachedCallBack);
   ros::Publisher speedPub = n.advertise<std_msgs::Int16>("linear_speed", 10);
 
-  calibrationProgress.waitingForUserSwitchPress = true;
-  calibrationProgress.forwardDepressed = false;
-  calibrationProgress.rearDepressed = false;
+  ros::Rate loop_rate(10);
 
+  ROS_INFO("waiting for user input");
+  
   while (ros::ok())
   {
     std_msgs::Int16 linearSpeedCmd;
