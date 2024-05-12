5ffb12320b9aff06d4e62d32d787656f1bdecb22
jclundy <joseph.lundy456@gmail.com>
2024-05-12 11:28:06 -0400

    tweaks open loop logic to allow more sensible collision slowdown and also setting of max speed parameter

--- catkin_ws/src/foos_control/src/openLoop.cpp
+++ catkin_ws/src/foos_control/src/openLoop.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/Int16.h"
+#include "std_msgs/UInt16.h"
 #include "std_msgs/UInt8.h"
 #include "sensor_msgs/Joy.h"
 #include "foos_control/GetLinearCalibration.h"
@@ -9,7 +10,7 @@
 
 #include <math.h>
 
-#define LINEAR_MAX_SPEED 7500
+#define DEFAULT_LINEAR_MAX_SPEED 7500
 #define WRIST_MAX_SPEED 5000
 #define KICK_SPEED 15000
 
@@ -22,13 +23,14 @@ static struct {
   int16_t maxLinearPos;
   int16_t minLinearPos;
   int16_t currentPos;
-  float alpha = 2000.0;
+  float alpha = 100.0;
+  uint16_t maxSpeed = DEFAULT_LINEAR_MAX_SPEED;
   
 } controlSettings;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-  setpoints.linearSpeed = LINEAR_MAX_SPEED*joy->axes[LH_JOY_VERTICAL_AXIS_INDEX];
+  setpoints.linearSpeed = controlSettings.maxSpeed * joy->axes[LH_JOY_VERTICAL_AXIS_INDEX];
   
   int16_t wristControlSpeed = -WRIST_MAX_SPEED*joy->axes[RH_JOY_HORIZONTAL_AXIS_INDEX];
   
@@ -44,6 +46,12 @@ void linearStepsCallBack(const std_msgs::Int16& msg)
 
 void setAlphaCallBack(const std_msgs::Int16& msg) {
 	controlSettings.alpha = (float) msg.data;
+  ROS_INFO("Buffer steps (aka alpha setting) : %f", controlSettings.alpha);
+}
+
+void setLinearMaxSpeedCallBack(const std_msgs::UInt16& msg) {
+  controlSettings.maxSpeed = msg.data;
+  ROS_INFO("Linear max speed setting : %i", controlSettings.maxSpeed);
 }
 
 int main(int argc, char **argv)
@@ -59,6 +67,7 @@ int main(int argc, char **argv)
   ros::Subscriber joySub = n.subscribe<sensor_msgs::Joy>("joy", 10, joyCallback);
   ros::Subscriber stepsSub = n.subscribe("linear_steps", 10, linearStepsCallBack);
   ros::Subscriber alphaSub = n.subscribe("set_alpha", 10, setAlphaCallBack);
+  ros::Subscriber linearMaxSpeedSub = n.subscribe("set_linear_max_speed", 10, setLinearMaxSpeedCallBack);
   
   ros::ServiceClient client = n.serviceClient<foos_control::GetLinearCalibration>("linear_calibration_info");
 
@@ -70,8 +79,16 @@ int main(int argc, char **argv)
     
     controlSettings.maxLinearPos = srv.response.max;
     controlSettings.minLinearPos = srv.response.min;
+    const int16_t stepRange = controlSettings.maxLinearPos - controlSettings.minLinearPos;
+    const float rangeInMM = 88.0;
+    const float bufferDistanceInMM = 20.0; // MM
+    float bufferSteps =  bufferDistanceInMM * stepRange / rangeInMM;
     
-    ROS_INFO("Max : %i, Min : %i", controlSettings.maxLinearPos, controlSettings.minLinearPos);
+    controlSettings.alpha = bufferSteps;
+
+    ROS_INFO("Max : %i, Min : %i, Range : %i", controlSettings.maxLinearPos, controlSettings.minLinearPos, stepRange);
+    ROS_INFO("Buffer steps (aka alpha setting) : %f", controlSettings.alpha);
+    ROS_INFO("Linear max speed setting : %i", controlSettings.maxSpeed);
     
   } else {
     ROS_INFO("Failed to call service linear_calibration_info");
@@ -95,7 +112,7 @@ int main(int argc, char **argv)
     int16_t distance = (speedCommand > 0)? (controlSettings.maxLinearPos - controlSettings.currentPos) : (controlSettings.currentPos - controlSettings.minLinearPos);  
     float scale = tanh(fabs(distance/(controlSettings.alpha)));
          
-    linearSpeedCmd.data = speedCommand * scale;
+    linearSpeedCmd.data = roundf(speedCommand * scale);
     wristSpeedCmd.data = setpoints.wristSpeed;
     linearSpeedPub.publish(linearSpeedCmd);
     wristSpeedPub.publish(wristSpeedCmd);
